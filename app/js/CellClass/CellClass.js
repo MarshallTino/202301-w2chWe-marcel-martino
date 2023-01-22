01bfbef9a5502b8bd52373e5ee8f2b3a67d5c983
Marcel Martino <marcelmartino2053@gmail.com>
2023-01-22 16:28:06 +0100

    add function to find alive cells

--- app/js/CellClass/CellClass.js
+++ app/js/CellClass/CellClass.js
@@ -2,12 +2,33 @@ class Cell {
   alive;
   positionX;
   positionY;
+  willBeALive;
+  numberOfNeighbours;
 
   constructor(positionX, positionY, willBeALive) {
     this.alive = Math.random() < 0.7;
     this.positionX = positionX;
     this.positionY = positionY;
     this.willBeALive = willBeALive;
+  }
+
+  findNeighbours(newBoard) {
+    let aliveNeighbours = 0;
+
+    for (let i = this.positionX - 1; i <= this.positionX + 1; i++) {
+      for (let j = this.positionY - 1; j <= this.positionY + 1; j++) {
+        if (i >= 0 && i < newBoard.length && j >= 0 && j < newBoard[0].length) {
+          if (
+            newBoard[i][j].alive &&
+            !(i === this.positionX && j === this.positionY)
+          ) {
+            aliveNeighbours++;
+          }
+        }
+      }
+    }
+
+    this.numberOfNeighbours = aliveNeighbours;
   }
 }
 
--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,7 @@
 import generateRandomBoard from "./generateRandomBoard/generateRandomBoard.js";
+import numberOfNeighbours from "./findNeighbours/findNeigbhours.js";
 
 const newBoard = generateRandomBoard(3, 3);
-
+console.table(newBoard);
+numberOfNeighbours(newBoard);
 console.table(newBoard);
